22993f71f0006b006e69691c527527ab816de91b
Mikihiko01 <tellad.adam@gmail.com>
2022-03-09 12:26:42 +0100

    nevek

---
+++ src/kozos/Kozos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kozos;
+
+/**
+ *
+ * @author Teller.A.Balazs
+ */
+public class Kozos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nev1,nev2;
+    }
+    
+}
